c9f1a51d76f28ed0506e2ef31863171b37fe224a
Aleksandr Pokatilov <pokatilov0802@gmail.com>
2024-02-11 15:34:37 +0100

    Ocaml🐫: Second code acquired

--- Ocaml/bin/main.ml
+++ Ocaml/bin/main.ml
@@ -1,12 +1,60 @@
 type memory = int array
 type registers = int array
-type stack = int Stack.t
+(* type stack = int Stack.t *)
 
 type state = {
     memory    : memory;
     registers : registers;
-    stack     : stack;
+    (* stack     : stack; *)
   }
+
+exception Machine_halt
+
+let is_value addr = 0 <= addr && addr <= 32767
+let is_register addr = 32768 <= addr && addr <= 32775
+let register_of_int i = i - 32768
+
+let read_mem state = function
+  | i when is_value i -> state.memory.(i)
+  | _ -> invalid_arg "memory address expected"
+
+let read_reg state = function
+  | i when is_register i ->
+     let reg = register_of_int i in
+     state.registers.(reg)
+  | _ -> invalid_arg "register address expected"
+
+(* let read_stack state = Stack.pop state.stack *)
+
+let read state = function
+  | i when is_value i -> i
+  | i when is_register i ->
+     read_reg state i
+  | _ -> invalid_arg "invalid address"
+
+let halt _state _pc = raise Machine_halt
+
+let out' state pc =
+  let value = read state (read_mem state (pc + 1)) in
+  let char  = char_of_int value in
+  print_char char;
+  pc + 2
+
+let noop _state pc = pc + 1
+
+let rec exec state pc =
+  let command = match read_mem state pc with
+    | 0 -> halt
+    | 19 -> out'
+    | 21 -> noop
+    | instr ->
+       Printf.eprintf "Warning: unknown instruction at %d: %d\n" pc instr;
+       noop
+    | exception Invalid_argument _ ->
+       Printf.eprintf "Error: command is not in memory: %d\n" pc;
+       failwith (Printf.sprintf "out of memory read: %d" pc)
+  in
+  exec state (command state pc)
 
 let input_value channel =
   try
@@ -30,9 +78,11 @@ let () =
     let state = {
         memory = input_program channel;
         registers = [|0; 0; 0; 0; 0; 0; 0; 0|];
-        stack = Stack.create ()
+        (* stack = Stack.create () *)
       } in
-    print_endline "Nothing here yet"
+    try exec state 0 with
+    | Machine_halt -> Printf.printf "Info: Machine halted!\n";
+    close_in channel
   with
   | Invalid_argument _ -> Printf.eprintf "Usage: %s <filename>\n" prog_name
   | Sys_error s -> Printf.eprintf "Error: %s\n" s
